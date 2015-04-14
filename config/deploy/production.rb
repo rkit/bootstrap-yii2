aae858f5295ff4b2ba59edf5d8ca3bb3dfc03022
Igor Romanov <rkit.ru@gmail.com>
2015-04-14 14:55:43 +0700

    Edit deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,12 @@
 set :stage, :production
+
+set :branch,    'master'
+set :deploy_to, ''
+set :domain,    ''
+set :url,	    'http://' + fetch(:domain)
+
+server fetch(:domain), user: '', roles: %w{web app},
+ssh_options: 
+{
+    forward_agent: true,
+}
