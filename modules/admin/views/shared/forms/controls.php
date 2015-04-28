72c19dea9faa4405095c1d46c999ea68367611b0
Igor Romanov <rkit.ru@gmail.com>
2015-04-28 17:58:58 +0700

    Fix form confirmation

--- modules/admin/views/shared/forms/controls.php
+++ modules/admin/views/shared/forms/controls.php
@@ -6,8 +6,8 @@ use yii\helpers\Html;
 <div class="form-controls">
     <div class="form-group pull-left">
         <?= Html::submitButton(Yii::t('app', 'Save'), [
-            'name' => 'submit', 
-            'class' => 'btn btn-info', 
+            'name' => 'submit',
+            'class' => 'btn btn-info',
             'data-loading-text' => Yii::t('app', 'Please wait…')
         ]) ?>
         <?php if (isset($model->primaryKey) && $model->primaryKey): ?>
@@ -15,15 +15,15 @@ use yii\helpers\Html;
         	['delete', 'id' => $model->primaryKey, 'reload' => true],
         	[
         		'title' => Yii::t('app', 'Delete'),
-        		'class' => 'confirmation btn btn-danger',
+        		'class' => 'btn btn-danger',
         		'data-pjax' => '1',
         		'data-method' => 'post',
-        		'data-confirmation' => Yii::t('app', 'Are you sure you want to delete this record?')
+        		'data-confirm' => Yii::t('app', 'Are you sure you want to delete this record?')
         	]
         ); ?>
         <?php endif?>
     </div>
-    
+
     <?php if (isset($model->date_create) && isset($model->date_update)): ?>
     <div class="form-inline pull-right text-right text-muted small">
         <div class="form-group">
