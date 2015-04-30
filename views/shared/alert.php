5c0179dae87a62734fe1101c604445549deaf7f5
Igor Romanov <rkit.ru@gmail.com>
2015-04-30 12:20:15 +0700

    Edit alert template

--- views/shared/alert.php
+++ views/shared/alert.php
@@ -3,6 +3,6 @@ use yii\helpers\Html;
 $email = Html::mailto(Yii::t('app', 'contact us'), Yii::$app->settings->emailMain);
 ?>
 <?= $message; ?>
-<p class="text-muted small">
-    <?= Yii::t('app', 'If you need help, please {email}', ['email' => $email]) ?>
-</p>
+<?php if($type == 'error'):?>
+<p class="text-muted small"><?= Yii::t('app', 'If you need help, please {email}', ['email' => $email]) ?></p>
+<?php endif?>
