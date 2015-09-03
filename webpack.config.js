4fb3c6267a21d63ed7daa5dd113354c8d86a2a6a
Igor Romanov <rkit.ru@gmail.com>
2015-09-03 14:07:54 +0700

    Improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,8 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var BowerWebpackPlugin = require('bower-webpack-plugin');
 var assetsDir = path.join(__dirname, './web/assets');
-var ENV_PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  context: path.join(__dirname, '.'),
-
   entry: {
     admin: './web/js/admin',
     front: './web/js/front'
@@ -21,8 +18,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-      { test: /\.(woff2|woff|svg|ttf|eot)([\?]?.*)$/, loader: 'file-loader?name=[name].[ext]' }
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+      },
+      {
+        test: /\.(woff2|woff|svg|ttf|eot)([\?]?.*)$/,
+        loader: 'file-loader?name=[name].[ext]',
+      },
     ],
 
     noParse: [
@@ -32,22 +35,18 @@ module.exports = {
 
   plugins: [
     new ExtractTextPlugin('[name].css'),
-
     new BowerWebpackPlugin({
       modulesDirectories: ['./vendor/bower'],
       manifestFiles: ['bower.json', '.bower.json'],
       includes: /.*/,
       excludes: /.*\.less$/
     }),
-
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
     }),
-
     new webpack.optimize.DedupePlugin(),
-
-    //new webpack.optimize.CommonsChunkPlugin("commons.js")
+    new webpack.optimize.OccurenceOrderPlugin(),
 
     function() {
       this.plugin('done', function(stats) {
