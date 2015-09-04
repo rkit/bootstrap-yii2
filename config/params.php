22062729e379d9688f4c4b488883f8b751f187fb
Igor Romanov <rkit.ru@gmail.com>
2015-09-04 14:07:47 +0700

    Edit config/params

--- config/params.php
+++ config/params.php
@@ -1,11 +1,7 @@
 <?php
 
-$assets = '1';
-
-if (YII_ENV !== 'test') {
-    $assets = @file_get_contents(__DIR__ . '/../web/assets/manifest.json');
-    $assets = (array)json_decode($assets);
-}
+$assets = @file_get_contents(__DIR__ . '/../web/assets/manifest.json');
+$assets = (array)json_decode($assets);
 
 return [
     'mainTimeZone' => 'Europe/Moscow',
