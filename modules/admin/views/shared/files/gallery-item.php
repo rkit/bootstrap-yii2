83d2ec08948aae4cff6bc9e4e3cb10e1e991328e
Igor Romanov <rkit.ru@gmail.com>
2015-10-01 21:51:40 +0700

    Edit gallery-item

--- modules/admin/views/shared/files/gallery-item.php
+++ modules/admin/views/shared/files/gallery-item.php
@@ -2,7 +2,9 @@
 use yii\helpers\Html;
 ?>
 <li>
-  <a href="<?= $file->path()?>" target="_blank"><img src="<?= $model->thumb('gallery', '80x80', $file->path())?>"></a>
+  <a href="<?= $file->path()?>" target="_blank">
+    <img src="<?= $model->thumb('gallery', '80x80', $file->path())?>">
+  </a>
   <a class="btn btn-lg"><span class="glyphicon glyphicon-remove remove-item" data-remove-item="li"></span></a>
   <?= Html::textInput(Html::getInputName($model, $attribute) . '[files][' . $file->id .']', $file->title, [
       'class' => 'form-control',
