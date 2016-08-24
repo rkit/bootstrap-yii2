40dc0d2479e83eabfe3affceba61488fcf030541
Igor Romanov <rkit.ru@gmail.com>
2016-08-24 11:07:46 +0700

    Refactor `Http` helper

--- helpers/Http.php
+++ helpers/Http.php
@@ -19,16 +19,20 @@ class Http
             case 400:
                 $msg = $msg ? $msg : Yii::t('app.messages', 'Bad request');
                 throw new \yii\web\BadRequestHttpException($msg);
+                break;
             case 401:
                 $msg = $msg ? $msg : Yii::t('app.messages', 'Access is unauthorized');
                 throw new \yii\web\UnauthorizedHttpException($msg);
+                break;
             case 403:
                 $msg = $msg ? $msg : Yii::t('app.messages', 'Access Denied');
                 throw new \yii\web\ForbiddenHttpException($msg);
+                break;
             case 404:
             default:
                 $msg = $msg ? $msg : Yii::t('app.messages', 'Page not found');
                 throw new \yii\web\NotFoundHttpException($msg);
+                break;
         } // @codeCoverageIgnore
     }
 }
