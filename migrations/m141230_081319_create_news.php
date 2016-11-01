e8a8251d13378adfb010bb0b32d96f85c14d2307
Igor Romanov <rkit.ru@gmail.com>
2016-11-01 10:43:13 +0700

    Fix migration for news

--- migrations/m141230_081319_create_news.php
+++ migrations/m141230_081319_create_news.php
@@ -7,9 +7,9 @@ class m141230_081319_create_news extends Migration
 {
     public function safeUp()
     {
-        //
-        // News
-        //
+        /**
+         * News
+         */
         $this->createTable('{{%news}}', [
             'id' => Schema::TYPE_PK,
             'type_id' => Schema::TYPE_INTEGER . " NOT NULL DEFAULT 0",
@@ -27,30 +27,30 @@ class m141230_081319_create_news extends Migration
         $this->createIndex('type_id', '{{%news}}', 'type_id');
         $this->createIndex('status', '{{%news}}', 'status');
 
-        //
-        // Types
-        //
+        /**
+         * Types
+         */
         $this->createTable('{{%news_type}}', [
             'id' => Schema::TYPE_PK,
             'title' => Schema::TYPE_STRING . " NOT NULL",
         ], $this->tableOptions);
 
-        //
-        // Tags
-        //
-        $this->createTable('{{%news_tag_assn}}', [
+        /**
+         * Tags
+         */
+        $this->createTable('{{%news_tags}}', [
             'news_id' => Schema::TYPE_INTEGER . " NOT NULL",
             'tag_id' => Schema::TYPE_INTEGER . " NOT NULL",
         ], $this->tableOptions);
 
-        $this->addPrimaryKey('key', '{{%news_tag_assn}}', 'news_id, tag_id');
+        $this->addPrimaryKey('key', '{{%news_tags}}', 'news_id, tag_id');
 
-        $this->addForeignKey('fk_news_tag', '{{%news_tag_assn}}', 'news_id', '{{%news}}', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_news_tag', '{{%news_tags}}', 'news_id', '{{%news}}', 'id', 'CASCADE', 'CASCADE');
     }
 
     public function safeDown()
     {
-        $this->dropTable('{{%news_tag_assn}}');
+        $this->dropTable('{{%news_tags}}');
         $this->dropTable('{{%news_type}}');
         $this->dropTable('{{%news}}');
     }
