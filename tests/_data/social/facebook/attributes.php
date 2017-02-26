baf1acbd387a0cee18f3b001a78fbc22fee066d5
Igor Romanov <rkit.ru@gmail.com>
2017-02-26 15:41:55 +0700

    Fix tests

--- tests/_data/social/facebook/attributes.php
+++ tests/_data/social/facebook/attributes.php
@@ -8,7 +8,7 @@ return [
     'verified' => true,
     'picture' => [
         'data' => [
-            'url' => 'https://scontent.xx.fbcdn.net/v/t1.0-1/c15.0.50.50/p50x50/10354686_10150004552801856_220367501106153455_n.jpg?oh=f518a1d4d5a6b640944e8e6f8395e5d6&oe=584B932F',
+            'url' => 'http://vk.com/images/camera_50.png',
         ],
     ],
 ];
