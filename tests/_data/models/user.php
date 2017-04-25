ff5631fda40ba64eb3b64e1eead0bce7bdd9b2d2
Igor Romanov <rkit.ru@gmail.com>
2017-04-25 13:32:50 +0700

    Upd test data

--- tests/_data/models/user.php
+++ tests/_data/models/user.php
@@ -20,7 +20,7 @@ return [
     'user-2' => [
         'username' => 'user-2',
         'email' => 'user-2@example.com',
-        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbNd',
+        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbN2',
         // password: 123123
         'password' => '$2y$13$2c0xt9QwWVq1yBUPmWl3ZeD/poVF8cyrwWrX87suGrYyRbP47Y1Mq',
         'password_reset_token' => 'bPDnNGI85L4va3dYJ_0xoz-Kw7NtzloS_' . time(),
@@ -33,7 +33,7 @@ return [
     'user-3' => [
         'username' => 'user-3',
         'email' => 'user-3@example.com',
-        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbNd',
+        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbN3',
         // password: 123123
         'password' => '$2y$13$2c0xt9QwWVq1yBUPmWl3ZeD/poVF8cyrwWrX87suGrYyRbP47Y1Mq',
         'password_reset_token' => 'cPDnNGI85L4va3dYJ_0xoz-Kw7NtzloS_' . time(),
@@ -46,11 +46,11 @@ return [
     'user-4' => [
         'username' => 'user-4',
         'email' => 'user-4@example.com',
-        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbNd',
+        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbN4',
         // password: 123123
         'password' => '$2y$13$2c0xt9QwWVq1yBUPmWl3ZeD/poVF8cyrwWrX87suGrYyRbP47Y1Mq',
-        'password_reset_token' => 'dPDnNGI85L4va3dYJ_0xoz-Kw7NtzloS_' . time(),
-        'email_confirm_token' => 'd7QKA1EkFe0cNoUJ7hIwDPDtdQtrQ7JY_' . time(),
+        'password_reset_token' => 'dPDnNGI85L4va3dYJ_0xoz-Kw7Ntzlo4_' . time(),
+        'email_confirm_token' => 'd7QKA1EkFe0cNoUJ7hIwDPDtdQtrQ7J4_' . time(),
         'date_create' => '2015-01-04 12:02:00',
         'date_update' => '2015-01-04 12:02:00',
         'ip' => '2130706434',
@@ -59,11 +59,11 @@ return [
     'user-5' => [
         'username' => 'user-5',
         'email' => 'user-5@example.com',
-        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbNd',
+        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbN5',
         // password: 123123
         'password' => '$2y$13$2c0xt9QwWVq1yBUPmWl3ZeD/poVF8cyrwWrX87suGrYyRbP47Y1Mq',
         'password_reset_token' => '123_0',
-        'email_confirm_token' => 'd7QKA1EkFe0cNoUJ7hIwDPDtdQtrQ7JY_' . time(),
+        'email_confirm_token' => 'd7QKA1EkFe0cNoUJ7hIwDPDtdQtrQ7J5_' . time(),
         'date_create' => '2015-01-05 12:02:00',
         'date_update' => '2015-01-05 12:02:00',
         'ip' => '2130706435',
@@ -73,11 +73,11 @@ return [
     'user-6' => [
         'username' => 'user-6',
         'email' => 'user-6@example.com',
-        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbNd',
+        'auth_key' => 'xFK_r79Q976mtxqccblijO-SmqjBwbN6',
         // password: 123123
         'password' => '$2y$13$2c0xt9QwWVq1yBUPmWl3ZeD/poVF8cyrwWrX87suGrYyRbP47Y1Mq',
-        'password_reset_token' => 'dPDnNGI85L4va3dYJ_0xoz-Kw7NtzloS_' . time(),
-        'email_confirm_token' => 'd7QKA1EkFe0cNoUJ7hIwDPDtdQtrQ7JY_' . time(),
+        'password_reset_token' => 'dPDnNGI85L4va3dYJ_0xoz-Kw7Ntzlo6_' . time(),
+        'email_confirm_token' => 'd7QKA1EkFe0cNoUJ7hIwDPDtdQtrQ7J6_' . time(),
         'date_create' => '2015-01-06 12:02:00',
         'date_update' => '2015-01-06 12:02:00',
         'ip' => '2130706436',
