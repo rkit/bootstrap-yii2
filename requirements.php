94db6099700c6222c0a919d22b787b7502a7360a
Igor Romanov <rkit.ru@gmail.com>
2017-04-26 10:57:49 +0700

    Upd `requirements.php`

--- requirements.php
+++ requirements.php
@@ -7,42 +7,44 @@
  *
  * In order to run this script from the web, you should copy it to the web root.
  * If you are using Linux you can create a hard link instead, using the following command:
- * ln requirements.php ../requirements.php
+ * ln ../requirements.php requirements.php
  */
+
 // you may need to adjust this path to the correct Yii framework path
 $frameworkPath = dirname(__FILE__) . '/vendor/yiisoft/yii2';
+
 if (!is_dir($frameworkPath)) {
     echo '<h1>Error</h1>';
     echo '<p><strong>The path to yii framework seems to be incorrect.</strong></p>';
-    echo '<p>You need to install Yii framework via composer or adjust 
-    the framework path in file <abbr title="' . __FILE__ . '">' . basename(__FILE__) . '</abbr>.</p>';
-    echo '<p>Please refer to the <abbr title="' . dirname(__FILE__) . '/README.md">README</abbr> 
-    on how to install Yii.</p>';
+    echo '<p>You need to install Yii framework via composer or adjust the framework path in file <abbr title="' . __FILE__ . '">' . basename(__FILE__) . '</abbr>.</p>';
+    echo '<p>Please refer to the <abbr title="' . dirname(__FILE__) . '/README.md">README</abbr> on how to install Yii.</p>';
 }
+
 require_once($frameworkPath . '/requirements/YiiRequirementChecker.php');
 $requirementsChecker = new YiiRequirementChecker();
-$gdMemo = $imagickMemo = 'Either GD PHP extension with FreeType support or ImageMagick 
-PHP extension with PNG support is required for image CAPTCHA.';
+
+$gdMemo = $imagickMemo = 'Either GD PHP extension with FreeType support or ImageMagick PHP extension with PNG support is required for image CAPTCHA.';
 $gdOK = $imagickOK = false;
+
 if (extension_loaded('imagick')) {
     $imagick = new Imagick();
     $imagickFormats = $imagick->queryFormats('PNG');
     if (in_array('PNG', $imagickFormats)) {
         $imagickOK = true;
     } else {
-        $imagickMemo = 'Imagick extension should be installed with PNG support in 
-        order to be used for image CAPTCHA.';
+        $imagickMemo = 'Imagick extension should be installed with PNG support in order to be used for image CAPTCHA.';
     }
 }
+
 if (extension_loaded('gd')) {
     $gdInfo = gd_info();
     if (!empty($gdInfo['FreeType Support'])) {
         $gdOK = true;
     } else {
-        $gdMemo = 'GD extension should be installed with FreeType support in 
-        order to be used for image CAPTCHA.';
+        $gdMemo = 'GD extension should be installed with FreeType support in order to be used for image CAPTCHA.';
     }
 }
+
 /**
  * Adjust requirements according to your application specifics.
  */
@@ -81,16 +83,7 @@ $requirements = array(
         'mandatory' => false,
         'condition' => extension_loaded('memcache') || extension_loaded('memcached'),
         'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-caching-memcache.html">MemCache</a>',
-        'memo' => extension_loaded('memcached') ? 'To use memcached set 
-        <a href="http://www.yiiframework.com/doc-2.0/yii-caching-memcache.html#$useMemcached-detail">
-            MemCache::useMemcached
-        </a> to <code>true</code>.' : ''
-    ),
-    array(
-        'name' => 'APC extension',
-        'mandatory' => false,
-        'condition' => extension_loaded('apc'),
-        'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-caching-apccache.html">ApcCache</a>',
+        'memo' => extension_loaded('memcached') ? 'To use memcached set <a href="http://www.yiiframework.com/doc-2.0/yii-caching-memcache.html#$useMemcached-detail">MemCache::useMemcached</a> to <code>true</code>.' : ''
     ),
     // CAPTCHA:
     array(
@@ -125,9 +118,20 @@ $requirements = array(
     'phpSmtp' => array(
         'name' => 'PHP mail SMTP',
         'mandatory' => false,
-        'condition' => strlen(ini_get('SMTP'))>0,
+        'condition' => strlen(ini_get('SMTP')) > 0,
         'by' => 'Email sending',
         'memo' => 'PHP mail SMTP server required',
     ),
 );
+
+// OPcache check
+if (!version_compare(phpversion(), '5.5', '>=')) {
+    $requirements[] = array(
+        'name' => 'APC extension',
+        'mandatory' => false,
+        'condition' => extension_loaded('apc'),
+        'by' => '<a href="http://www.yiiframework.com/doc-2.0/yii-caching-apccache.html">ApcCache</a>',
+    );
+}
+
 $requirementsChecker->checkYii()->check($requirements)->render();
