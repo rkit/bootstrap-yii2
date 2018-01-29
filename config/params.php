ecf1c71a24da5f52f4c63c65e89fcf1e6fa7ccd2
Igor Romanov <rkit.ru@gmail.com>
2018-01-29 22:44:08 +0700

    Fix host

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => 'http://bootstrap-yii2.dev',
+    'host' => 'http://bootstrap-yii2.test',
     'user.tokenExpire' => 3600,
     'user.rateLimitMax' => 100,
     'user.rateLimitTime' => 30,
